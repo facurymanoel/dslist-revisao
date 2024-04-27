eb3ed0ef32e6c7d749c09a9e9e91c4096198622c
dalmo <facurymanoel10@gmail.com>
2024-04-27 17:39:48 -0300

    primeiro commit

---
+++ src/main/java/com/devsuperior/dslist/DslistRevisaoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistRevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistRevisaoApplication.class, args);
+	}
+
+}
