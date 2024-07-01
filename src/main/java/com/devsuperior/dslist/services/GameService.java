d31442158ae929c412a9e8e210b048d9bdee0e3a
dalmo <facurymanoel10@gmail.com>
2024-07-01 14:03:33 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -6,26 +6,34 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameService {
-	
+
 	@Autowired
 	private GameRepository gameRepository;
-	
-	public List<GameMinDTO> findAll(){
-		
+
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll() {
+
 		List<Game> result = gameRepository.findAll();
-		
-		return result.stream()
-				     .map(x -> new ModelMapper()
-				     .map(x, GameMinDTO.class))
-				     .collect(Collectors.toList());
-		 
-		 }
+
+		return result.stream().map(x -> new ModelMapper().map(x, GameMinDTO.class)).collect(Collectors.toList());
+
+	}
 
 }
