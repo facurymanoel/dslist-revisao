dee7a25c02c300f5e5a47f9d37a879a90b8e8865
Dalmo Facuri <mffacuri@terra.com.br>
2026-09-19 14:35:48 -0300

    Domain model

---
+++ src/main/java/com/devs/superior/dslist/entities/Belonging.java
@@ -0,0 +1,50 @@
+package com.devs.superior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_belonging")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Belonging {
+
+	@EmbeddedId
+	private BelongingPk id = new BelongingPk();
+
+	private Integer position;
+
+	public Belonging(Game game, GameList list, Integer position) {
+
+		id.setGame(game);
+		id.setList(list);
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belonging other = (Belonging) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/devs/superior/dslist/entities/BelongingPk.java
@@ -0,0 +1,48 @@
+package com.devs.superior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class BelongingPk {
+	
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	private Game game;
+	
+	@ManyToOne
+	@JoinColumn(name = "list_id")
+	private GameList list;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, list);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPk other = (BelongingPk) obj;
+		return Objects.equals(game, other.game) && Objects.equals(list, other.list);
+	}
+	
+	
+	
+
+}
